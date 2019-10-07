e9f9e2aafb0a6f64701954e7c714783a2acc7c4d
Steve <pickleman20@hotmail.com>
2019-10-07 18:25:27 -0400

    added to mymalloc() and move initializing node0 to outside the function

--- mymalloc.c
+++ mymalloc.c
@@ -3,15 +3,19 @@
 #include "mymalloc.h"
 static char myblock[4096]; //Initialize the data
 
-struct node
+//Format for metadata before each allocated data in myblock
+typedef struct _metadata
 {
-//# of bytles until next node/end of memory
-unsigned short int size;
-//# of bytes allocated with malloc
-unsigned short int used;
+	//# of bytles until next node/end of memory
+	unsigned short int size;
+	//# of bytes allocated with malloc
+	unsigned short int used;
+} metadata;
 
-};
-//Setup Node0 here
+//Setup node0
+metadata* node0 = &(myblock[0]);
+node0->used = 0;
+node0->size = 4096 - sizeof(metadata);
 
 /* Malloc function
  * Scans starting from 0
@@ -34,41 +38,41 @@ unsigned short int used;
  * 		else (if no more nodes)
  * 			return NULL
  */
-
-
- 
-void* mymalloc(size_t bytes, char* fileName, int line) {
-
-	//Malloc of 0 bytes should result in NULL
-	//Can return null since it is a void pointer
-	
-	//need a way to see if it is the first time called
-	//not sure if this works, check later	 
-	static int called = 0; 	
-
-	if (bytes == 0)
-		{
+void* mymalloc(size_t bytes, char* fileName, int line)
+{
+	printf("Attempting to allocate %zu bytes\n", bytes);
+	if (bytes == 0) //allocating 0 bytes, return NULL
+	{
+		printf("\tReturning NULL, allocated 0 bytes\n");
 		return NULL; 	
-		}
-
-	if (bytes < 0 )
-		{
-		//ERROR Do stuff
-		
-		}
-//	static struct node *first;
-	static struct node *first;	
-	//if first time called
-	if (called == 0){
-		
-		//create the first node at the start of the memory
-		first = (struct node*) myblock;
-		first->size = 4096;
-		printf("size aka: %d", first->size);			
 	}
+	//Note: size_t is an unsigned decimal, cannot be negative
+	
+	metadata* currentNode = node0;
+	while (/*temp, replace with something else later*/ true)
+	{
+		if (currentNode->size >= bytes)
+		{
+			//Current node has enough space to allocate
+			if (currentNode->used == 0) 
+			{
+				currentNode->used = (unsigned short)bytes;
+			}
+		} else {
+			//Current node is too small, continue to next node
 
-	
-	
+			//Next node cannot exist, no space
+			if ((void*)currentNode == (void*)myBlock + 4096)
+			{
+				printf("\tReturning NULL, no more space\n");
+				return NULL;
+			}
+
+			currentNode = (void*)currentNode + currentNode->size + sizeof(metadata);
+			//continue to check next node
+			continue;
+		}
+	}
 }
 
 /* Free function
@@ -88,5 +92,6 @@ void* mymalloc(size_t bytes, char* fileName, int line) {
  * 	set currentNode to Node0
  * 	set Node0.size to Node0.size + metadataSize(of Node1) + Node1.Size
  */
-void myfree(void* ptr, char* fileName, int line) {	
+void myfree(void* ptr, char* fileName, int line) {
+	
 }
